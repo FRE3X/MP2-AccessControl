12660e00d85258cc0c9bda21b9d26a6640fed5ab
FRE3X <Callot.antoine@gmail.com>
2014-12-12 15:12:44 +0100

    Test PCSC3: Reussite de la recuperation de l'UID

--- Test_PCSC/Test_PCSC3/test_PCSC.cpp
+++ Test_PCSC/Test_PCSC3/test_PCSC.cpp
@@ -5,7 +5,7 @@
 #include <string.h>
 
 using namespace std; 
-
+#define RcvLenMax 300l                       // Max. APDU Buffer length.
 void main(){
 	long lRetValue; // valeur de retour des fonction
 	SCARDCONTEXT		m_hContext;	// Adresse du buffer ou on stocke le context récupéré
@@ -13,12 +13,19 @@ void main(){
 	DWORD				cch = SCARD_AUTOALLOCATE;//le nombre de caractere
 	SCARDHANDLE			m_hCard;
 	DWORD				m_dwActiveProtocol;
+	LPCSCARD_IO_REQUEST  ioRequest = SCARD_PCI_T1;
 	
 	wchar_t				m_szSelectedReader[256] = {'A','C','S',' ','A','C','R','1','2','2',' ','0',0};// nom du lecteur
 	
+	BYTE baCmdApduGetData[] = { 0xFF, 0xCA, 0x00, 0x00, 0x00};//commande 
+	BYTE baResponseApdu[300];//buffer de reponse
+	DWORD lResponseApduLen = 0; //tailler buffer
+
 	WORD *PtWord;
 	byte Valeur; // La valeur de mon caractere
-	
+
+	lResponseApduLen = RcvLenMax; 	
+
 	////////////////Etablisement du context////////////////
 	lRetValue = SCardEstablishContext(
 		SCARD_SCOPE_USER, // Le contexte est un contexte utilisateur, et toutes les opérations de base sont effectuées dans le domaine de l'utilisateur.
@@ -63,6 +70,16 @@ void main(){
 					SCARD_PROTOCOL_Tx, //Preferred protocols (T=0 or T=1).
 					&m_hCard,               // Returns the card handle.
 					&m_dwActiveProtocol);
+	
+	
+	////////////////Transmission ////////////////
+	lRetValue = SCardTransmit(m_hCard,		// Card handle.
+							ioRequest,		// Pointer to the send protocol header.
+							baCmdApduGetData,	// Send buffer.
+							(DWORD)sizeof(baCmdApduGetData),	// Send buffer length.
+							NULL,			// Pointer to the rec. protocol header.
+							baResponseApdu,	// Receive buffer.
+							&lResponseApduLen);	// Receive buffer length.
 	
 	getchar(); 
 }
