c7fee6a2bf9a5c4f7da7b4f245304a4a64eb23f3
FRE3X <Callot.antoine@gmail.com>
2014-12-12 11:14:23 +0100

    test PCSC3: test de la fonction connect

---
+++ Test_PCSC/Test_PCSC3/test_PCSC.cpp
@@ -0,0 +1,68 @@
+#include <winscard.h>
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+#include <string.h>
+
+using namespace std; 
+
+void main(){
+	long lRetValue; // valeur de retour des fonction
+	SCARDCONTEXT		m_hContext;	// Adresse du buffer ou on stocke le context récupéré
+	char*				pmszReaders = NULL; // pointeur sur la premiere case du tableau alloue.
+	DWORD				cch = SCARD_AUTOALLOCATE;//le nombre de caractere
+	SCARDHANDLE			m_hCard;
+	DWORD				m_dwActiveProtocol;
+	
+	char				m_szSelectedReader[256] = {'A','C','S',' ','A','C','R','1','2','2',' ','0',0};// nom du lecteur
+	
+	WORD *PtWord;
+	byte Valeur; // La valeur de mon caractere
+	
+	////////////////Etablisement du context////////////////
+	lRetValue = SCardEstablishContext(
+		SCARD_SCOPE_USER, // Le contexte est un contexte utilisateur, et toutes les opérations de base sont effectuées dans le domaine de l'utilisateur.
+		NULL,             
+		NULL,             
+		&m_hContext);	 // Adresse du buffer ou on stocke le context récupéré
+	
+	
+	//verifications :
+	if(lRetValue == 0){
+		cout << "Connection ok" << endl ;
+	}else
+	{
+		cout << "Erreur : " << lRetValue ; 
+	}
+
+	////////////////recuperation du nom ////////////////
+	lRetValue = SCardListReaders(
+                    m_hContext,             // Resource manager handle. 
+                    NULL, // NULL: list all readers in the system 
+                    (LPTSTR)&pmszReaders, // Returs the card readers list.
+                    &cch );
+	
+	PtWord = (WORD*)pmszReaders; //initialisation de PtWord
+	int i = 0;
+	
+	//affichage du nom :
+	while (i < cch)
+	{
+		Valeur = (byte)*PtWord;
+		cout << Valeur ;
+		PtWord++;
+		i++; 
+	}
+	cout << endl ; 
+
+	////////////////connection ////////////////
+	lRetValue = SCardConnect( 
+                   m_hContext, // Adresse du buffer ou on stocke le context récupéré
+					(LPCWSTR)m_szSelectedReader,     // Reader name.
+					SCARD_SHARE_EXCLUSIVE,  // Share Mode.
+					SCARD_PROTOCOL_Tx, //Preferred protocols (T=0 or T=1).
+					&m_hCard,               // Returns the card handle.
+					&m_dwActiveProtocol);
+	
+	getchar(); 
+}
--- Test_PCSC/test_PCSC2/Test_PCSC.cpp
+++ Test_PCSC/test_PCSC2/Test_PCSC.cpp
@@ -4,24 +4,28 @@
 #include <iostream>
 #include <string.h>
 
-using namespace std ; 
+using namespace std ;
+//#define Macro_Status(lRetValue,msg)
+//if(lRetValue == SCARD_S_SUCCESS){}else{}
 
 void main(){
-	LPTSTR          pmszReaders = NULL;
+	char*         pmszReaders = NULL;
 	LPTSTR          pszReader;
-	LPTSTR			pszaReaders[16];
+	char		pszaReaders[16];
 	DWORD           cch = SCARD_AUTOALLOCATE;
 	int				iNumberOfReaders;
 	int 			iSelectedReader;
 	SCARDCONTEXT		m_hContext;	
 	long				lRetValue;
-	
+	SCARDHANDLE			m_hCard;
+	DWORD				m_dwActiveProtocol;
+	char				m_szSelectedReader[256] = {'A','C','S',' ','A','C','R','1','2','2',0};// nom du lecteur
 	LPWSTR			monreader;
 	
 
 	//LPDWORD			size;
 	
-	////////////////connection////////////////
+	////////////////Etablisement du context////////////////
 	lRetValue = SCardEstablishContext(
 		SCARD_SCOPE_USER, // Le contexte est un contexte utilisateur, et toutes les opérations de base sont effectuées dans le domaine de l'utilisateur.
 		NULL,             
@@ -34,37 +38,62 @@ void main(){
 	{
 		cout << "Erreur : " << lRetValue ; 
 	}
-	
-	/*
+		
 
-	////////////////lecture ////////////////
+	////////////////recuperation du nom ////////////////
 	//methode récupérant le nom du lecteur connecter : 
 	lRetValue = SCardListReaders(
                     m_hContext,             // Resource manager handle. 
                     NULL, // NULL: list all readers in the system 
                     (LPTSTR)&pmszReaders, // Returs the card readers list.
                     &cch );
+	
+	iNumberOfReaders = 0;
+	
 
-	LPTSTR monbuffer = pmszReaders; 
-	int i = 0; 
-	while (i != strlen(pmszReaders))
+
+	while ( *pszReader != '\0' )
 	{
-		//monbuffer[i] = pmszReaders[i];
-		cout << pmszReaders[i] << endl; 
-		i++; 
+		printf("\n      Reader [%2d] %s", iNumberOfReaders, pszReader);
+		pszaReaders[ iNumberOfReaders ] = *pszReader;// = (LPTSTR)pszReader;
+		pszReader = pszReader + strlen((char*)pszReader) + 1;
+		iNumberOfReaders++;
 	}
+	byte* PtChar;
+	byte Valeur;
+	WORD *PtWord;
+	PtChar = (byte*)pmszReaders;
+	PtWord = (WORD*)pmszReaders;
+	PtChar++;
+	PtChar++;	
+
+	Valeur = (byte)*PtWord;
+	PtWord++;
+	Valeur = (byte)*PtWord;
+	PtWord++;
+	Valeur = (byte)*PtWord;
+
 	getchar();
 	
-	*/
+	//strcpy(m_szSelectedReader, (char*)pszaReaders[0]);
+
+	////////////////connection ////////////////
+	lRetValue = SCardConnect( 
+                    m_hContext, // Resource manager handle.
+					(LPCWSTR)m_szSelectedReader,     // Reader name.
+					SCARD_SHARE_EXCLUSIVE,  // Share Mode.
+					SCARD_PROTOCOL_Tx, //Preferred protocols (T=0 or T=1).
+					&m_hCard,               // Returns the card handle.
+					&m_dwActiveProtocol);
+	
 
 	////////////////recuperation UID ////////////////
 	BYTE commande[] = {0xFF,0xCA,0x00,0x00,0x0A}; 
-	SCARDHANDLE		hcard ;
 	DWORD			taille = 0x05;  
 	BYTE			reception[30];
-	LPDWORD			tailleReception; 
+	DWORD			tailleReception =6; 
 	
-	lRetValue = SCardTransmit(hcard,NULL,commande,taille,NULL,reception,tailleReception);
+	lRetValue = SCardTransmit(m_hContext,NULL,commande,taille,NULL,reception,&tailleReception);
 	
 	getchar();
 }
